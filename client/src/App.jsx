54922f1be068e802df83d557b95e38f85ff85116
Shreyas Kulkarni <skulkarni.sk.18@gmail.com>
2024-01-03 22:39:11 -0800

    Commit Completed! - Till register page functionality.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import IndexPage from './pages/indexPage.jsx';
 import LoginPage from './pages/loginPage.jsx';
+import RegisterPage from './pages/RegisterPage.jsx';
 import Layout from "./Layout";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Layout/>}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegisterPage/>}/>
       </Route>
     </Routes>
     
